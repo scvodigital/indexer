05addc50d5915c673a9b8fd4c74474c3f9e9b9cc
Matthew Wilkes <tonicblue@gmail.com>
2019-09-13 11:06:31 +0100

    Removing travis stuff and trying out Github Actions

--- publish.js
+++ publish.js
@@ -7,7 +7,7 @@ const admin = require('firebase-admin');
 const isGzip = require('is-gzip');
 const stringify = require('json-stringify-safe');
 
-const firebaseServiceAccount = require('./secret.json');
+const firebaseServiceAccount = JSON.parse(process.env.GOOGLE_APPLICATION_CREDENTIALS);
 
 const FIREBASE_ROOT = '/sites-new/';
 const BUILD_DIR = path.join(__dirname, 'build');
