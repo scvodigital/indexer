29bb289beafb4701bbed758cabe1963db0c2c660
Matthew Wilkes <tonicblue@gmail.com>
2019-09-13 13:10:13 +0100

    Added configuration, assets, and webpack

---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const JsonIncWebpackPlugin = require('./json-inc-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const ExtraWatchWebpackPlugin = require('extra-watch-webpack-plugin');
+
+module.exports = (env) => {
+  const plugins = [
+    new CopyWebpackPlugin([
+      {
+        from: '**/*',
+        to: './build/',
+        context: './assets/'
+      }
+    ], { debug: 'warning' }),
+    new JsonIncWebpackPlugin({
+      pattern: './configuration/config.inc.json',
+      output: './build/config.json'
+    }),
+  ];
+
+  if (env !== 'prod') {
+    plugins.push(new ExtraWatchWebpackPlugin({
+      files: ['./configuration/**/*', './assets/**/*']
+    }));
+  }
+
+  return {
+    watchOptions: {
+      ignored: ['node_modules', 'build'],
+      aggregateTimeout: 300
+    },
+    entry: [
+      './src/main.js',
+    ],
+    output: {
+      filename: 'build/main.js',
+      library: 'Indexer',
+      libraryTarget: 'var'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          loader: 'babel-loader' + (!env === 'prod' ? '?cacheLoader' : ''),
+          query: {
+            presets: ['@babel/preset-env'],
+            compact: false
+          }
+        }
+      ]
+    },
+    plugins: plugins
+  };
+};
